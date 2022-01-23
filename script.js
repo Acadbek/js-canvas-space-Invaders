056b15624b703ed6e0e96d00f1ca58b7a276333d
Asadbek <a.nosirjonov@uic.group>
2022-01-23 15:58:32 +0500

    refactor: player

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ class Player {
             y: 0
         }
 
+        this.rotation = 0
+
         const image = new Image();
         image.src = './images/spacecraft.png';
         image.onload = () => {
@@ -28,8 +30,14 @@ class Player {
     }
 
     draw() {
+
+        c.save()
+        c.translate(player.position.x + player.width / 2, player.position.y + player.height / 2);
+        c.rotate(this.rotation);
+        c.translate(-player.position.x - player.width / 2, -player.position.y - player.height / 2);
         if (this.image)
             c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+        c.restore()
     }
 
     update() {
@@ -59,12 +67,15 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
 
-    if (keys.a.pressed) {
-        player.velocity.x = -5
-    } else if (keys.d.pressed) {
-        player.velocity.x = 5
+    if (keys.a.pressed && player.position.x >= 0) {
+        player.velocity.x = -7
+        player.rotation = -0.15
+    } else if (keys.d.pressed && player.position.x + player.width <= canvas.width) {
+        player.velocity.x = 7
+        player.rotation = 0.15
     } else {
         player.velocity.x = 0
+        player.rotation = 0
     }
 
 }
